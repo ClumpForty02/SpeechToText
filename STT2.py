48e8bab42a38e61fa5d6e4c83ca674bac7a2d4b9
Mokshada Mohapatra <134252481+ClumpForty02@users.noreply.github.com>
2024-03-08 01:05:15 +0530

    Update STT2.py

--- STT2.py
+++ STT2.py
@@ -1,17 +1,19 @@
 import speech_recognition
+import speech_recognition as sr
+import pyaudio
 import pyttsx3
 
-recognizer = speech_recognition.Recognizer()
+recognizer = sr.Recognizer()
 
 while True:
 
     try:
-        with speech_recognition.Microphone() as mic:
+        with sr.Microphone() as mic:
             recognizer.adjust_for_ambient_noise(mic, duration=0.2)
             print("Say something.....")
             audio = recognizer.listen(mic)
         try:
-            text = recognizer.recognize_(audio)
+            text = recognizer.recognize_google(audio)
             text = text.lower()
 
             print(f"Recognized text here: {text}")
